4bd0d6b38b18c056aee310d4459054edb5805515
wcjameson <williamjameson90@gmail.com>
2022-01-21 12:51:11 -0800

    WIP

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-Describe: replaceNum()
+Describe: range()
 
 Test: "It should return an array with a 0 if the number 0 is inputted"
-Code: replaceNum(0)
-Expected Output: 0
+Code: range(0)
+Expected Output: [0]
 
-Test: 
+Test: "It should return array with elements 0 to user input number"
+Code: range(5)
+Expected Output: [0, 1, 2, 3, 4, 5]
+
+Test: "It should return array with modified elements, i.e. "Beep!" instead of 1"
+Code: range(5)
+Expected Output: [0, "Beep!", 2, 3, 4, 5]
--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,30 @@ function range(num) {
   for (let i = 0; i <= num; i++) {
       ans.push(i);
   }
+  ans.forEach(function(element) {
+    if (element.toString().includes("1")) {
+      //element.replace("1", "Beep!")
+      console.log("AHHH!!")
+    } 
+  });
   return ans;
+};
+
+
+
+
+
+
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
 }
